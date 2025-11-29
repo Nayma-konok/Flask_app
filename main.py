47cf1d3387b28ed69a0018ea176183fdc18c67e1
Nayma-konok <naymakonok002@gmail.com>
2025-11-29 00:21:03 +0000

    Creating Database

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 
+
+app.config["SECRET_KEY"]="myapplication123"   #this guard the app from hackers
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///data.db"
+db=SQLAlchemy(app)
+
+class Form(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    date = db.Column(db.String(20))  # store date as string for simplicity
+    occupation = db.Column(db.String(80))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -15,4 +28,6 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
