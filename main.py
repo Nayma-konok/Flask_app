790a643f1523a4cc61bd4493d353a9c7b4173c19
Nayma-konok <naymakonok002@gmail.com>
2025-12-01 22:42:57 +0000

    Improved message formatting sent to users after form submission

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+MAIL_USERNAME=os.getenv("mail_username")
+MAIL_PASSWORD=os.getenv("mail_password")
 app=Flask(__name__)
 
 
-app.config["SECRET_KEY"]="myapplication123"   
+app.config["SECRET_KEY"]="myapplication123"   #this guard the app from hackers
 app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///data.db"
+app.config["MAIL_SERVER"]="smtp.gmail.com"
+app.config["MAIL_PORT"]="465"
+app.config["MAIL_USE_SSL"]=True
+app.config["MAIL_USERNAME"]=MAIL_USERNAME
+app.config["MAIL_PASSWORD"]=MAIL_PASSWORD
+
+
 db=SQLAlchemy(app)
+mail=Mail(app)
 
 
 class Form(db.Model):
@@ -33,6 +48,17 @@ def index():
                   email=email, date=date_obj, Occupation=Occupation)
         db.session.add(form)
         db.session.commit()
+
+        message_body=f"{first_name}, thank you for your submission \n"\
+                        f"here is your data: {first_name}\n{last_name}\n{date}\n"\
+                        f"Thank you.."
+
+        message=Message(subject="New form submission", sender=app.config["MAIL_USERNAME"],
+                        recipients=[email], body=message_body)
+        
+        mail.send(message)
+
+        flash(f"{first_name},your form was submitted successfully!", "success")
 
     return render_template("index.html")
 
